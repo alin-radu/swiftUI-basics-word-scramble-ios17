537e76b91cda68fdba22182b483ea93babaaa16d
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 20:40:11 +0200

    Initial Commit

---
+++ WordScramble-ios17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScramble-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble-ios17/WordScramble_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_ios17App.swift
+//  WordScramble-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
